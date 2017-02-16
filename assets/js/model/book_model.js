857540b99102d346a5ebd1fc2ff485cf5d825f0d
arivin <btc022003@gmail.com>
2017-02-16 17:41:22 +0800

    实现分页取数据功能

--- assets/js/model/book_model.js
+++ assets/js/model/book_model.js
@@ -1,5 +1,9 @@
-var BookModel = Backbone.Model.extend({})
+//创建书籍模型
+var BookModel = Backbone.Model.extend({
+    idAttribute:'_id'
+})
 
+//创建书籍集合
 var BookCollection = Backbone.Collection.extend({
     model:BookModel,
     url:'http://localhost:3000/api/v1/books/'
--- assets/js/views/list_view.js
+++ assets/js/views/list_view.js
@@ -1,4 +1,4 @@
-var Book = require('../model/book_model')
+var Book = require('../model/book_model')//引入书籍model文件
 var Model = Book.Model
 var Collection = Book.Collection
 
@@ -6,9 +6,11 @@ var tpl = require('../../tpl/list.html') //视图模版
 
 module.exports = Backbone.View.extend({
     initialize:function(type){
+        this.pageIndex = 1
+        this.type = type
         this.books = new Collection()
         this.listenTo(this.books,'add',this.dataChangedHandle)//监听集合的add事件,在fetch之后触发
-        this.books.fetch({data:{type:type}})
+        this.books.fetch({data:{type:this.type,page:this.pageIndex},remove:false})
     },
     template:template.compile(tpl),
     events:{
@@ -18,10 +20,12 @@ module.exports = Backbone.View.extend({
         this.$el.html(this.template({data:this.books.toJSON()}))
         return this
     },
-    dataChangedHandle:function(){
+    dataChangedHandle:function(model){
         this.render()
     },
     loadMoreData:function(){
-        this.books.add(new Model({title:"从你的全世界路过"}))
+        //this.books.add(new Model({title:"从你的全世界路过"}))
+        this.pageIndex += 1
+        this.books.fetch({data:{type:this.type,page:this.pageIndex},remove:false})
     }
 })
